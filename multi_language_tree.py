12c65895fcc49f3da387004f727a5e8a560388f6
Ananya Pramod <ananyapramod7@gmail.com>
2024-07-08 23:17:46 +0530

    Create vanilla class multi_language_tree

---
+++ multi_language_tree.py
@@ -0,0 +1,12 @@
+class Word:
+    def __init__(self, text = "", language = "", translations = []):
+        self.text = text
+        self.language = language
+        self.translations = []
+    
+    # Adds a new node given by text, language and adds the list of translations to the node created
+    def add_word(self, text: str, language: str, translations:  list) -> None:
+        pass
+    
+    # Similarly other class functions can be added here
+
